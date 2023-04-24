7ebc4a995c2dfe58bca45107ac1d4cf239cff1ea
kimmeatan <happymandong@naver.com>
2023-04-24 10:34:47 +0900

    updated

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # -*- coding:UTF-8 -*-
 import streamlit as st
+import pandas as pd
 
 def main():
     st.title("Hello World")
@@ -32,6 +33,18 @@ def main():
     #Help
     st.help(range)
     st.help(st.title)
+
+    #데이터 불러오기
+    iris = pd.read_csv('data/iris.csv')
+
+    st.title('IRIS 테이블')
+    st.dataframe(iris, 500,100) # Height, Width
+
+    st.title('table()')
+    st.table(iris)
+
+    st.title('write()')
+    st.write(iris)
 
 
 if __name__ == '__main__':
